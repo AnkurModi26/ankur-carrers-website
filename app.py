ec2d5470c9e0b2f80a18b277a273e81a12c7875b
AnkurModi26 <74940431+AnkurModi26@users.noreply.github.com>
2023-05-01 16:27:31 +0000

    Added Job Openings

--- app.py
+++ app.py
@@ -2,10 +2,31 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [{
+  'id': 1,
+  'title': 'Data Anaylst',
+  'location': 'Banglore, India',
+  'Salary': 'Rs.1,00,000'
+}, {
+  'id': 2,
+  'title': 'Data Scentis',
+  'location': 'Delhi, India',
+  'Salary': 'Rs.15,00,000'
+}, {
+  'id': 3,
+  'title': 'Front-End Enginner ',
+  'location': 'Remote, India',
+}, {
+  'id': 4,
+  'title': 'Back-End Enginner ',
+  'location': 'San-francisco, USA',
+  'Salary': '$120,000'
+}]
+
 
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS, company_name="IT")
 
 
 app.run(host='0.0.0.0', debug=True)
